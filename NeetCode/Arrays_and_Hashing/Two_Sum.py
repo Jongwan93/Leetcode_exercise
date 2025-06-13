5342b841fc7663eb2b12979dbdd289ef0b7b1254
Jongwan93 <jwhur1993@gmail.com>
2025-06-13 00:20:39 -0400

    one more

---
+++ NeetCode/Arrays_and_Hashing/Two_Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums, target):
+        myDict = dict()
+        for i, num in enumerate(nums):
+            other = target - num
+            print(f"num print: {num}")
+            print(f"other print: {other}")
+            if other in myDict:
+                return [myDict[other], i]
+            myDict[num] = i
+
+def main():
+    sol = Solution()
+    nums = [3, 4, 5, 6]
+    target = 7
+    print(sol.twoSum(nums, target))
+
+if __name__ == "__main__":
+    main()
