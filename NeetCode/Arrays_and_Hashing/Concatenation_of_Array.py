f622ece649a46c5c9b5ddeb448aa8644b81da56a
Jongwan93 <jwhur1993@gmail.com>
2025-06-12 23:27:40 -0400

    one more and folder recontructed

---
+++ NeetCode/Arrays_and_Hashing/Concatenation_of_Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getConcatenation(self, nums):
+        ans = []
+        for i in range(0, len(nums)*2):
+            ans.append(nums[i%len(nums)])
+        return ans
+
+def main():
+    sol = Solution()
+    nums = [1, 4, 1, 2]
+    print(sol.getConcatenation(nums))
+
+if __name__ == "__main__":
+    main()
