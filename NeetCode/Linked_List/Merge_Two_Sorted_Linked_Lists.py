1127e83ff5e223efd4ad6430a301f1883df25dd8
Jongwan93 <jwhur1993@gmail.com>
2025-08-20 00:11:27 -0400

    Merge two linked list fixed

--- NeetCode/Linked_List/Merge_Two_Sorted_Linked_Lists.py
+++ NeetCode/Linked_List/Merge_Two_Sorted_Linked_Lists.py
@@ -6,22 +6,38 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1, list2):
-        current1 = list1
-        current2 = list2
-        sortedHead = None
-
         if not list1:
             return list2
         
         if not list2:
             return list1
+        
+        dummy = ListNode(0)
+        # tail will iterate through new list
+        # dummy must stay pointing at first node
+        tail = dummy
 
-        while current1 and current2:
-            if current2.value <= current1.value:
-                next2 = current2.next
-                current2.next = current1
-                current2 = next2
-                current1 = current1.next
+        # regardless of list1 and list2 structure,
+        # simply compare the two nodes and connect the node with smaller value.
+        while list1 and list2:
+            if list2.val <= list1.val:
+                tail.next = list2
+                list2 = list2.next
+            else:
+                tail.next = list1
+                list1 = list1.next
+            tail = tail.next
+        
+        # if list1 and list2 is not fully iterated, connect it to the tail
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        
+        return dummy.next
+                
+
+# https://neetcode.io/problems/merge-two-sorted-linked-lists?list=neetcode250
 
 
         
