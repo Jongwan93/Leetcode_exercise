181fadff529fa8fb307f22a1a82b1ed5257bd51b
Jongwan93 <jwhur1993@gmail.com>
2025-08-26 22:05:28 -0400

    another tree

--- NeetCode/Trees/Binary_Tree_Inorder_Traversal.py
+++ NeetCode/Trees/Binary_Tree_Inorder_Traversal.py
@@ -6,4 +6,20 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root):
+        result = []
+        stack = []
+        current = root
+
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
         
+        return result
+
+
+
+# https://neetcode.io/problems/binary-tree-inorder-traversal?list=neetcode250
